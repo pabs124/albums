84ef75403933e19c5d835cb9fe20398e6e30c31c
Sahil Pabby <pabs124@gmail.com>
2018-12-19 00:11:00 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Text, AppRegistry} from 'react-native'
+
+const App = () => {
+    return (
+        <Text>Some Text</Text>
+    )
+}
+
+AppRegistry.registerComponent('albums', () => App)
